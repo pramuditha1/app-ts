71a3495c858244a86f0c58c451b29f12a3e781aa
pramuditha <pramuditha1@live.com>
2023-07-16 22:45:32 +0530

    Create a Todos class using typescript and construct todos array

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
+import Todos from './components/todos';
+import Todo from './models/todo';
 
 function App() {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
+
   return (
     <div className="App">
-      
+      <Todos items={todos}/>
     </div>
   );
 }
---
+++ src/components/todos.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import Todo from '../models/todo';
+
+const Todos: FC<{items: Todo[]}> = (props) => {
+    return (
+        <ul>
+            {
+                props.items.map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                ))
+            }
+        </ul>
+    )
+}
+
+export default Todos;
