833a51fb595aa5f7da96464ee74ac026fa5365d9
pramuditha <pramuditha1@live.com>
2023-07-17 05:15:39 +0530

    TodoItem reusable component implement

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,5 @@
+export const TodoItem:React.FC<{text: string}> = (props) => {
+  return (
+    <li>{props.text}</li>
+   )
+ }
--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react'
 import Todo from '../models/todo';
+import { TodoItem } from './TodoItem';
 
 const Todos: FC<{items: Todo[]}> = (props) => {
     return (
         <ul>
             {
                 props.items.map((item) => (
-                    <li key={item.id}>{item.text}</li>
+                    <TodoItem text={item.text}></TodoItem>
                 ))
             }
         </ul>
